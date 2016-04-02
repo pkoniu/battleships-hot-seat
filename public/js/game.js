62e524915b6a6c2d6c60aea36480a8e4a8f656a7
Patryk Konior <patryk.konior@gmail.com>
2016-04-03 00:46:09 +0200

    hover over table handler

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   console.log("Game started!");
+  var activePlayer = 'player-one'; //todo: changes dynamically during play
 
   $('table').click(function(event) {
     var clickedTile = event.target;
